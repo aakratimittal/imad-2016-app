15c1745194550a5cd3cfafb9fc72b2a05f8baa5d
aakratimittal <am741@snu.edu.in>
2016-10-15 00:49:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/Article_1',function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Article_1.html'));
+
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
